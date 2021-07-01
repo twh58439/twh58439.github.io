99e749b3ddc3f368326ef8fc29b273a57d054a79
Thomas Horn <hortw1991@gmail.com>
2021-07-01 10:35:02 -0400

    added welcome page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,27 +1,23 @@
 <template>
- <div>
-  <b-navbar type="dark" variant="dark">
-    <b-navbar-nav>
-      <b-nav-item href="#">Home</b-nav-item>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Deliverables" right>
+          <b-dropdown-item href="#">D1</b-dropdown-item>
+          <b-dropdown-item href="#">D2</b-dropdown-item>
+        </b-nav-item-dropdown>
 
-      <!-- Navbar dropdowns -->
-      <b-nav-item-dropdown text="Deliverables" right>
-        <b-dropdown-item href="#">D1</b-dropdown-item>
-        <b-dropdown-item href="#">D2</b-dropdown-item>
-      </b-nav-item-dropdown>
-
-      <b-nav-item href="#">About</b-nav-item>
-    </b-navbar-nav>
-  </b-navbar>
-</div> 
+        <b-nav-item href="#">About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
-  <b-container fluid>
-    <h1>CSCI 4800 - Final Project Page</h1>
+  <b-container fluid class="m-3 d-flex justify-content-center">
+		<h1>CSCI 4800 - Final Project Page</h1>
   </b-container>
 </template>
 
