0d104a66abb23b64102b1663e83c0b2edceab897
Eric Kim <erick4920@gmail.com>
2021-07-23 14:23:25 -0400

    Provided proper attributions

--- components/three/EmbeddedFigure.vue
+++ components/three/EmbeddedFigure.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <h5>High Fidelity Mockups</h5>
+    <h2>High Fidelity Mockups</h2>
     <p class="font-weight-light"></p>
     <b-embed
       style="border: 1px solid rgba(0, 0, 0, 0.1)"
       src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F91gfn5iJUE7TXOGmOVR9tO%2FTerm-Project%3Fnode-id%3D7%253A897%26scaling%3Dscale-down%26page-id%3D0%253A1"
       allowfullscreen
     ></b-embed>
+    <p class="text-secondary text-center" style="font-size: 0.75rem">
+      Created using the
+      <a href="https://tinyurl.com/yjrkx2yt">Microsoft Teams UI Kit</a>
+    </p>
   </div>
 </template>
