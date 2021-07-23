9ce04ee17d6c44d0789e3fa56fdbce8aaa5366e2
Eric Kim <erick4920@gmail.com>
2021-07-23 13:30:34 -0400

    Fixed the embed

--- components/three/EmbeddedFigure.vue
+++ components/three/EmbeddedFigure.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-	<h5>High Fidelity Mockups</h5>
-	<p class="font-weight-light"></p>
+    <h5>High Fidelity Mockups</h5>
+    <p class="font-weight-light"></p>
     <b-embed
       style="border: 1px solid rgba(0, 0, 0, 0.1)"
-      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F91gfn5iJUE7TXOGmOVR9tO%2FTerm-Project%3Fnode-id%3D7%253A897%26scaling%3Dmin-zoom%26page-id%3D0%253A1"
+      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F91gfn5iJUE7TXOGmOVR9tO%2FTerm-Project%3Fnode-id%3D7%253A897%26scaling%3Dscale-down%26page-id%3D0%253A1"
       allowfullscreen
     ></b-embed>
   </div>
